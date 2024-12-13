26cd00f2628dbfab696ddf0133f7ac991a6f6c8e
renhai-lab <3152498252@qq.com>
2024-12-14 01:58:15 +0800

    在 build.sh 中添加静态文件压缩步骤

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
+python manage.py compress
 # 第一次运行： 创建超级用户
 # echo "Creating superuser..."
 # python manage.py shell <<EOF
