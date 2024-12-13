63f4ab400b5bfa53335346156e346d3b9ede47a2
renhai-lab <3152498252@qq.com>
2024-12-14 01:51:07 +0800

    更新 build.sh，注释掉创建超级用户的代码；更新 render.yaml，禁用多个配置项的值同步

--- build.sh
+++ build.sh
@@ -11,18 +11,18 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-# 创建超级用户
-echo "Creating superuser..."
-python manage.py shell <<EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='${DJANGO_SUPERUSER_USERNAME}').exists():
-    User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')
-    print("Superuser created")
-else:
-    print("Superuser already exists")
-EOF
-if [ $? -ne 0 ]; then
-    echo "Failed to create superuser" >&2
-    exit 1
-fi
+# 第一次运行： 创建超级用户
+# echo "Creating superuser..."
+# python manage.py shell <<EOF
+# from django.contrib.auth import get_user_model
+# User = get_user_model()
+# if not User.objects.filter(username='${DJANGO_SUPERUSER_USERNAME}').exists():
+#     User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')
+#     print("Superuser created")
+# else:
+#     print("Superuser already exists")
+# EOF
+# if [ $? -ne 0 ]; then
+#     echo "Failed to create superuser" >&2
+#     exit 1
+# fi
